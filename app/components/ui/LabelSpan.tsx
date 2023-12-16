2b2a47f42fb2481307e58fe3fcd69771a70b7289
Tene Romaric <tenrom142@gmail.com>
2023-12-16 02:35:31 +0100

    search

---
+++ app/components/ui/LabelSpan.tsx
@@ -0,0 +1,9 @@
+import { cn } from "../../lib/utils";
+import React from "react";
+
+export default function LabelSpan({
+  className,
+  ...props
+}: React.HtmlHTMLAttributes<HTMLSpanElement>) {
+  return <span {...props} className={cn("font-semibold", className)} />;
+}
---
+++ app/components/ui/ValueSpan.tsx
@@ -0,0 +1,11 @@
+import { cn } from "../../lib/utils";
+import React from "react";
+
+export default function ValueSpan({
+  className,
+  ...props
+}: React.HtmlHTMLAttributes<HTMLSpanElement>) {
+  return (
+    <span {...props} className={cn("font-semibold text-gray-500", className)} />
+  );
+}
